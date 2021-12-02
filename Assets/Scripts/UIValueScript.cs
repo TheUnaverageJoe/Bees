cfe1ad412e4c275093c0c0bc24eba611a41ef22b
jwakeGit <jwake@ucsc.edu>
2021-12-01 16:28:59 -0800

    Fixed compilation errors

--- Assets/Scripts/UIValueScript.cs
+++ Assets/Scripts/UIValueScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIValueScript : MonoBehaviour
 {
@@ -8,19 +9,19 @@ public class UIValueScript : MonoBehaviour
     private GameObject slider;
     private GameObject hive;
     private HiveBehavior hiveScript;
+    private float sliderValue;
+    private float queueLength;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         slider = GameObject.Find ("UISlider");
         hive = GameObject.Find ("Hive");
         hiveScript = hive.GetComponent <HiveBehavior>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         sliderValue = slider.GetComponent <Slider>().value;
         queueLength = hiveScript.BeeQueue.Count;
-        totalValue = sliderValue * queueLength;
+        totalValue = (int)(sliderValue * queueLength);
     }
 }
