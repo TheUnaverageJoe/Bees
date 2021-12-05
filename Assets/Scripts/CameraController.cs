5bcfcf1db43623d5d9fe5041850d747a3f4079da
jwakeGit <jwake@ucsc.edu>
2021-12-04 17:17:57 -0800

    Slider Independent from Camera Movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class CameraController : MonoBehaviour
 {
@@ -33,50 +34,52 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Adding the a cleaner zomo feature for the mouse scroll wheel. 
-        //It has two locked heights for it can't go too high or below the ground. 
-        if(Input.mouseScrollDelta.y != 0) {
-            if(Input.mouseScrollDelta.y > 0 && newZoom.z < maxZoom) {
-                newZoom += Input.mouseScrollDelta.y * zoomAmount;
+        if (EventSystem.current.IsPointerOverGameObject() == false){
+            //Adding the a cleaner zomo feature for the mouse scroll wheel. 
+            //It has two locked heights for it can't go too high or below the ground. 
+            if(Input.mouseScrollDelta.y != 0) {
+                if(Input.mouseScrollDelta.y > 0 && newZoom.z < maxZoom) {
+                    newZoom += Input.mouseScrollDelta.y * zoomAmount;
+                }
+                if(Input.mouseScrollDelta.y < 0 && newZoom.z > minZoom) {
+                    newZoom += Input.mouseScrollDelta.y * zoomAmount;
+                }
             }
-            if(Input.mouseScrollDelta.y < 0 && newZoom.z > minZoom) {
-                newZoom += Input.mouseScrollDelta.y * zoomAmount;
+            //Adding mouse input for the middle mouse or left click to drag and move the camera
+            if(Input.GetMouseButtonDown(2) || Input.GetMouseButtonDown(0)) {
+                Plane tempPlane = new Plane(Vector3.up, Vector3.zero);
+                Ray tempRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+                float entryPoint;
+                if(tempPlane.Raycast(tempRay, out entryPoint)) {
+                    dragStartPosition = tempRay.GetPoint(entryPoint);
+                }
             }
-        }
-        //Adding mouse input for the middle mouse or left click to drag and move the camera
-        if(Input.GetMouseButtonDown(2) || Input.GetMouseButtonDown(0)) {
-            Plane tempPlane = new Plane(Vector3.up, Vector3.zero);
-            Ray tempRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            float entryPoint;
-            if(tempPlane.Raycast(tempRay, out entryPoint)) {
-                dragStartPosition = tempRay.GetPoint(entryPoint);
+            if(Input.GetMouseButton(2)|| Input.GetMouseButton(0)) {
+                Plane tempPlane = new Plane(Vector3.up, Vector3.zero);
+                Ray tempRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+                float entryPoint;
+                if(tempPlane.Raycast(tempRay, out entryPoint)) {
+                    dragCurrentPosition = tempRay.GetPoint(entryPoint);
+                    newPosition = transform.position + dragStartPosition - dragCurrentPosition;
+                }
             }
-        }
-        if(Input.GetMouseButton(2)|| Input.GetMouseButton(0)) {
-            Plane tempPlane = new Plane(Vector3.up, Vector3.zero);
-            Ray tempRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-            float entryPoint;
-            if(tempPlane.Raycast(tempRay, out entryPoint)) {
-                dragCurrentPosition = tempRay.GetPoint(entryPoint);
-                newPosition = transform.position + dragStartPosition - dragCurrentPosition;
+            //Adding mouse input for the right click to drag and rotate the camera. 
+            if(Input.GetMouseButtonDown(1)) {
+                rotateStartPosition = Input.mousePosition;
             }
-        }
-        //Adding mouse input for the right click to drag and rotate the camera. 
-        if(Input.GetMouseButtonDown(1)) {
-            rotateStartPosition = Input.mousePosition;
-        }
-        if(Input.GetMouseButton(1)) {
-            rotateCurrentPosition = Input.mousePosition;
-            Vector3 tempDiff = rotateStartPosition - rotateCurrentPosition;
-            rotateStartPosition = rotateCurrentPosition;
-            newRotation *= Quaternion.Euler(Vector3.up * (-tempDiff.x/5f));
-        }
-        //Adding the left shift as a multiplier of 2 for the pan speed. This can be adjusted 
-        if (Input.GetKey("left shift")) {
-            movementSpeed = fastSpeed;
-        }
-        else {
-            movementSpeed = normalSpeed;
+            if(Input.GetMouseButton(1)) {
+                rotateCurrentPosition = Input.mousePosition;
+                Vector3 tempDiff = rotateStartPosition - rotateCurrentPosition;
+                rotateStartPosition = rotateCurrentPosition;
+                newRotation *= Quaternion.Euler(Vector3.up * (-tempDiff.x/5f));
+            }
+            //Adding the left shift as a multiplier of 2 for the pan speed. This can be adjusted 
+            if (Input.GetKey("left shift")) {
+                movementSpeed = fastSpeed;
+            }
+            else {
+                movementSpeed = normalSpeed;
+            }
         }
         //If the player moves the cursor on the border or presses a wasd key then the camera will move given the pan speed
         //removed from if statement: || Input.mousePosition.y >= Screen.height - panBorderThickness
